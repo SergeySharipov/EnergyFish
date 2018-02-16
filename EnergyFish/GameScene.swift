f8128d10e685f42910c992d59f9f5995df8ab5ca
Sergey <sharipov14@gmail.com>
2018-02-16 16:37:11 -0500

    Add coin, fish impulse, bit mask for physics objects.

--- EnergyFish/GameScene.swift
+++ EnergyFish/GameScene.swift
@@ -9,28 +9,88 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
+class GameScene: SKScene,SKPhysicsContactDelegate {
+    let noCategory:UInt32 = 0
+    let hookCategory:UInt32 = 0b1
+    let fishCategory:UInt32 = 0b1 << 1
+    let coinCategory:UInt32 = 0b1 << 2
+    let borderCategory:UInt32 = 0b1 << 3
+    
+    var fish:SKSpriteNode?
+    
+    
+    func random() -> CGFloat{
+        return CGFloat(Float(arc4random()) / 0xFFFFFFFF)
+    }
+    
+    func random(min : CGFloat, max : CGFloat) -> CGFloat{
+        return random() * (max - min) + min
+    }
     
     override func didMove(to view: SKView) {
         
+        self.physicsWorld.contactDelegate = self
+        
+        fish = self.childNode(withName: "fish") as? SKSpriteNode
+        fish?.physicsBody?.categoryBitMask = fishCategory
+        fish?.physicsBody?.collisionBitMask = borderCategory
+        fish?.physicsBody?.contactTestBitMask = hookCategory | coinCategory | borderCategory
+        
+        let coin = self.childNode(withName: "coin") as? SKSpriteNode
+        coin?.physicsBody?.categoryBitMask = coinCategory
+        coin?.physicsBody?.collisionBitMask = noCategory
+        coin?.physicsBody?.contactTestBitMask = fishCategory
+        
+        let hook = self.childNode(withName: "hook") as? SKSpriteNode
+        hook?.physicsBody?.categoryBitMask = hookCategory
+        hook?.physicsBody?.collisionBitMask = noCategory
+        hook?.physicsBody?.contactTestBitMask = fishCategory
+        
+        let border = self.childNode(withName: "border") as? SKSpriteNode
+        border?.physicsBody?.categoryBitMask = borderCategory
+        border?.physicsBody?.collisionBitMask = borderCategory
+        border?.physicsBody?.contactTestBitMask = fishCategory
         
     }
     
+    func didBegin(_ contact: SKPhysicsContact) {
+        let cA:UInt32 = contact.bodyA.categoryBitMask
+        let cB:UInt32 = contact.bodyB.categoryBitMask
+        
+        if cA == fishCategory || cB == fishCategory {
+            let otherNode:SKNode = (cA == fishCategory) ? contact.bodyB.node! : contact.bodyA.node!
+            playerDidCollide(with: otherNode)
+        }
+    
+    }
+    
+    func playerDidCollide(with other:SKNode) {
+        let otherCategory = other.physicsBody?.categoryBitMask
+        if otherCategory == coinCategory {
+            other.removeFromParent()
+        }
+        else if otherCategory == hookCategory {
+            other.removeFromParent()
+            fish?.removeFromParent()
+        }
+    }
     
     func touchDown(atPoint pos : CGPoint) {
-    
+        
     }
     
     func touchMoved(toPoint pos : CGPoint) {
-       
+   
     }
     
     func touchUp(atPoint pos : CGPoint) {
-
+fish?.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 40))
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+        //for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+        
+     
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -45,8 +105,7 @@ class GameScene: SKScene {
         for t in touches { self.touchUp(atPoint: t.location(in: self)) }
     }
     
-    
-    override func update(_ currentTime: TimeInterval) {
+    func moveBackground() {
         enumerateChildNodes(withName: "background", using: ({
             (node, error) in
             let bg = node as! SKSpriteNode
@@ -55,6 +114,10 @@ class GameScene: SKScene {
                 bg.position = CGPoint(x:bg.position.x + bg.size.width * 2, y:bg.position.y)
             }
         }))
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        moveBackground()
         
     }
 }
